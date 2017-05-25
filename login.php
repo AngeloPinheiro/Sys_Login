b1550c9158dcd911134061821236b97e540adc64
AngeloPinheiro <angelorpsantos@gmail.com>
2017-05-24 21:48:32 -0300

    Bug 'Bem-Vindo' corrigido
    Bug corrigido usando empty() e operadores ternários

--- login.php
+++ login.php
@@ -1,28 +1,37 @@
 <?php
+   
+//http://blog.ultimatephp.com.br/sistema-de-login-php/
    require 'login.html';
    require 'conexao.php';
-
+    
     try {
-        
-        if(isset($_POST["enviar"])){
-        $login = $_POST["login"];
-        $senha = $_POST["senha"];
-        $stmt = $PDO->prepare('SELECT login, senha FROM usuarios where login = :login AND senha = :senha');
-         $stmt->execute(array(
-             ':login' => $login,
-             ':senha' => $senha
-         )
-         );         
-         $consulta = $stmt->fetch(PDO::FETCH_ASSOC);
-         if($consulta['login'] != $login && $consulta['senha'] != $senha){
-            echo "<center> Login ou Senha incorretos!</center";
-         }else{
-            echo "<center> Bem-Vindo </center";
-         }
-        }
-    } catch(PDOException $e) {
-      echo 'Error: ' . $e->getMessage();
-    }
+    	
+    	if(isset($_POST["enviar"])){
+            
+            $login = isset($_POST["login"]) ? $_POST['login'] : '';
+            $senha = isset($_POST["senha"]) ? $_POST['senha'] : '';
+            
+            if (empty($login) || empty($senha)) {
+                echo "<center> Insira Login e Senha </center><br>";        
+            }else{           
+                $stmt = $PDO->prepare('SELECT login, senha FROM usuarios where login = :login AND senha = :senha');
+                $stmt->execute(array(
+                       ':login' => $login,
+                       ':senha' => $senha
+                                    )
+                    );
+                    $consulta = $stmt->fetch(PDO::FETCH_ASSOC);
+                    if($consulta['login'] != $login || $consulta['senha'] != $senha){
+                        echo "<center> Login ou Senha incorretos!</center";
+                    }else{
+                        echo "<center> Bem-Vindo </center";
+                    }    
+            }         
+    	}
+	}catch(PDOException $e) {
+	  echo 'Error: ' . $e->getMessage();
+	}
 
 
 ?>
+
